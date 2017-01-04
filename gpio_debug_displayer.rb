7bbc815cca79a961eb84ba11b15fafdff40f03a4
Mcdostone <yann.prono@telecomnancy.net>
2017-01-04 23:30:55 +0100

    blink the debug LED

--- gpio_debug_displayer.rb
+++ gpio_debug_displayer.rb
@@ -3,6 +3,8 @@ require 'pi_piper'
 
 class GpioDebugDisplayer
 
+	DELAY = 2
+
 	def initialize(pin_debug = 26)
 		@pin_debug = pin_debug
 	end
@@ -10,11 +12,8 @@ class GpioDebugDisplayer
 	def show
 		Thread.new do
    			pin = PiPiper::Pin.new(:pin => @pin_debug, :direction => :out) 
-			for i in 0..5
-				pin.off
-				sleep(1)
-				pin.on
-			end 
+			pin.on
+			sleep 2
 			pin.off
 		end
 	end
