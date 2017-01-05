7da5f6b8ddc9bb9ddc4ca9624029dcc4a2d999d4
Yann Prono <yann.prono@telecomnancy.net>
2017-01-05 20:05:05 +0100

    gmm

--- main.rb
+++ main.rb
@@ -7,7 +7,7 @@ require 'rufus-scheduler'
 scheduler = Rufus::Scheduler.new
 network = '192.168.1.*' 
 scanner = NetworkScanner.new(network)
-displayer = gpio_displayer.new(16)
+displayer = GpioDisplayer.new(16)
 
 scheduler.in '2s' do
 	"Scanning #{network} ...\n"
@@ -15,7 +15,7 @@ scheduler.in '2s' do
 
 	devices.each do |device|
 		puts device
-		displayer.show(devices.empty ? scanner.nb_devices : devices.length)
+		displayer.show(devices.empty? ? scanner.nb_devices : devices.length)
 	end
 	puts "\n\n"
 end
