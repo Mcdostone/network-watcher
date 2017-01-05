59023d15bd62025ac9297605268a87799d755f08
Yann Prono <yann.prono@telecomnancy.net>
2017-01-05 19:54:59 +0100

    Add scheduler

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ source "https://rubygems.org"
 gem 'ruby-nmap', '~> 0.8.0'
 gem 'ipaddress', '~> 0.8.3'
 gem 'pi_piper', '~> 2.0'
+gem 'rufus-scheduler', '~> 3.2'
--- main.rb
+++ main.rb
@@ -2,16 +2,17 @@ require './proxy_network_scanner'
 require './network_scanner'
 require './gpio_displayer'
 require 'pi_piper'
+require 'rufus-scheduler'
 
 
-network = '10.10.108.128/26'
-#displayer = GpioDisplayer.new
+scheduler = Rufus::Scheduler.new
+network = '192.168.1.*' 
 scanner = NetworkScanner.new(network)
-devices = scanner.scan
 
-devices.each do |device|
-	puts device
+scheduler.in '2m' do
+	devices = scanner.scan
+	devices.each do |device|
+		puts device
+		displayer.show(devices.empty ? scanner.nb_devices : devices.length)
+	end
 end
-
-puts scanner.nb_devices
-displayer.show(devices.empty ? scanner.nb_devices : devices.length)
